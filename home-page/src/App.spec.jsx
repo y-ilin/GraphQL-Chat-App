5fabdaf0d5e902b05c6bd11e1a2f35f30913d35d
Yi Lin Lee <yilinlee19@gmail.com>
2021-06-19 23:26:23 +1000

    build

---
+++ home-page/src/App.spec.jsx
@@ -0,0 +1,19 @@
+// Check that it renders a Chat app
+import React from "react";
+import { render, screen } from "@testing-library/react";
+// import App from "./App";
+
+// describe("Home page app", () => {
+//   it("renders a Chat app", () => {
+//     jest.mock(
+//       "chat/Chat",
+//       () => ({
+//         Chat: () => <div>Hi</div>,
+//       }),
+//       { virtual: true }
+//     );
+
+//     render(<App />);
+//     screen.debug();
+//   });
+// });
